091465a98ea7db52f041145c8a90c817e29d8df0
annika123 <annika15-6045@diu.edu.bd>
2025-02-19 10:46:53 +0600

    labtask1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,37 +10,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter UI",
+      title: 'Flutter UI',
       home: Scaffold(
         appBar: AppBar(
           title: Text("Flutter UI"),
         ),
         body: Container(
           height: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.memory,
-                    size: 60,
-                    color: Colors.red,
-                  ),
-                  SizedBox(width: 20), // Spacer between icon and text
-                  Text(
-                    "Flutter ROW",
-                    style: TextStyle(
-                      color: const Color.fromARGB(255, 144, 132, 25),
-                      fontSize: 40,
-                    ),
-                  ),
-                ],
+              Icon(
+                Icons.star,
+                color: Colors.yellow,
+                size: 60,
               ),
-              SizedBox(height: 40), // Space between row and rating
-              RatingWidget(),  // Rating stars widget
+              Icon(
+                Icons.star,
+                color: Colors.yellow,
+                size: 60,
+              ),
+              Icon(
+                Icons.star,
+                color: Colors.yellow,
+                size: 60,
+              ),
+              Icon(
+                Icons.star,
+                color: Colors.yellow,
+                size: 60,
+              ),
+              Icon(
+                Icons.star,
+                color: Colors.grey,
+                size: 60,
+              ),
             ],
           ),
         ),
@@ -49,45 +54,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class RatingWidget extends StatefulWidget {
-  @override
-  _RatingWidgetState createState() => _RatingWidgetState();
-}
-
-class _RatingWidgetState extends State<RatingWidget> {
-  int _rating = 0; // Default rating is 0
-
-  void _setRating(int rating) {
-    setState(() {
-      _rating = rating;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: List.generate(5, (index) {
-            return IconButton(
-              icon: Icon(
-                index < _rating ? Icons.star : Icons.star_border,
-                color: Colors.yellow,
-                size: 40,
-              ),
-              onPressed: () {
-                _setRating(index + 1);  // Rating starts from 1
-              },
-            );
-          }),
-        ),
-        SizedBox(height: 20),
-      ],
-    );
-  }
-}
 
 
 
